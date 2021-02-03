950d7e4f4a77b33c2f525a753a1545752b3a537a
felipemfm <felipemfm@gmail.com>
2021-02-03 16:55:15 +0900

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	php/alarm.php

--- index.php
+++ index.php
@@ -13,6 +13,16 @@
     <link rel="stylesheet" href="css/resetcss.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
           integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png">
+    <link rel="manifest" href="image/site.webmanifest">
+    <link rel="mask-icon" href="/image/favicon.ico" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body onload="document.alarm.reset();">
 
