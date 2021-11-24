65dd33c52cbc7cd5ad49d769e1c6f9e5b9ca0701
felipemfm <73625660+felipemfm@users.noreply.github.com>
2021-11-24 15:18:46 +0900

    Update non_user_check.php

--- non_user_check.php
+++ non_user_check.php
@@ -3,11 +3,11 @@ session_start();
 include_once "add/header.add.php";
 ?>
 <div class="container bg-white border rounded-3 mx-auto my-5 py-3" style="width: 500px">
-    <h3 class="text-center">アラーム確認</h3>
+    <h3 class="text-center">Alarm Check</h3>
     <?php include "add/error_handler.add.php";?>
     <form action="./includes/non_user_check.inc.php" method="post">
         <div class="form-group">
-            <h4 class="my-2 text-center">携帯番号を入力してくだい</h4>
+            <h4 class="my-2 text-center">Enter the registered Phonenumber</h4>
             <input type="text" class="input-group my-3" name="phone">
             <button type='submit' id='submit' name='submit' class='btn btn-primary mt-3'>送信</button>
         </div>
