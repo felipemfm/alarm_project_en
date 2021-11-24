d10a8fe854dcfa88b5c98dcfa77205e96a3dc721
felipemfm <73625660+felipemfm@users.noreply.github.com>
2021-11-24 15:21:22 +0900

    Update profile_edit.php

--- profile_edit.php
+++ profile_edit.php
@@ -8,30 +8,30 @@ if(!isset($_SESSION["userName"])){
 <?php include_once "add/header.add.php";?>
 
 <div class="container bg-white border rounded-3 mt-5 pb-3" style="width: 500px">
-    <h1 class="text-center" style="margin: 1em;">プロファイル編集</h1>
+    <h1 class="text-center" style="margin: 1em;">Profile Edit</h1>
     <?php include_once "add/error_handler.add.php" ?>
     <form action="includes/profile.inc.php" method="post">
         <div class="form-group ">
-            <h2>メール</h2>
+            <h2>Email/h2>
             <input type="text" name="email" class="form-control" >
-            <button class="btn btn-primary my-3" type="submit" name="submit">編集</button>
+            <button class="btn btn-primary my-3" type="submit" name="submit">Edit</button>
         </div>
     </form>
     <form action="includes/profile.inc.php" method="post">
         <div class="form-group ">
-            <h2>電話番号</h2>
+            <h2>Phonenumber</h2>
             <input type="text" name="phoneNumber" class="form-control" >
-            <button class="btn btn-primary my-3" type="submit" name="submit">編集</button>
+            <button class="btn btn-primary my-3" type="submit" name="submit">edit</button>
         </div>
     </form>
     <form action="includes/profile.inc.php" method="post">
         <div class="form-group ">
-            <h2>パスワード</h2>
-            <p>新パスワード</p>
+            <h2>Password</h2>
+            <p>New Password</p>
             <input type="password" name="newPwd" class="form-control" >
-            <p>新パスワード確認</p>
+            <p>New Password Confirmation</p>
             <input type="password" name="newPwdRepeat" class="form-control">
-            <p>現在パスワード</p>
+            <p>Current Password</p>
             <input type="password" name="pwd" class="form-control" >
             <button class="btn btn-primary mt-3" type="submit" name="submit">編集</button>
         </div>
