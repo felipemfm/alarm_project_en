b38becba4ab9a9ccaf8c7f4fa1e6923f46052921
felipemfm <73625660+felipemfm@users.noreply.github.com>
2021-11-24 14:50:28 +0900

    Update error_handler.add.php

--- add/error_handler.add.php
+++ add/error_handler.add.php
@@ -2,36 +2,36 @@
 if (isset($_GET["error"])) {
     echo "<div class='text-center alert alert-danger ' role='alert'>";
     if($_GET["error"] == "emptyInput") {
-        echo "入力してください";
+        echo "Please enter the required information";
     } else if ($_GET["error"] == "invalidUid") {
-        echo "インバリッドユーザID";
+        echo "Invalid Username";
     } else if ($_GET["error"] == "invalidEmail") {
-        echo "インバリッドユーザメール";
+        echo "Invalid Email Address";
     } else if ($_GET["error"] == "invalidPhoneNumber") {
-        echo "インバリッド電話番号";
+        echo "Invalid Phonenumber";
     } else if ($_GET["error"] == "pwdMatch") {
-        echo "パスワードが合わない";
+        echo "Password doesn't match";
     } else if ($_GET["error"] == "userTaken") {
-        echo "ユーザまたはメールはユーザー名は既に取られています";
+        echo "The entered Username or Email is already in used";
     }else if ($_GET["error"]=="sameAs"){
-        echo "発車駅と行先駅は異なければいけません";
+        echo "Departure and Destination stations can not be the same";
     }else if ($_GET["error"]=="unavailable"){
-        echo "選択不可";
+        echo "Unavailable at the Time";
     }else if($_GET["error"]=="wrongLogin") {
-        echo "ユーザー名またはパスワードの入力に誤りがある";
+        echo "The entered Username or Password are incorrect";
     }else if ($_GET["error"]=="notFound"){
-        echo "アクティブなアラームを見つけることができませんでした";
+        echo "Can not found an Active Alarm";
     }else if($_GET["error"]=="wrongPwd") {
-        echo "間違えたパスワード";
+        echo "The Password in incorrect";
     }
     echo "</div>";
 }
 if(isset($_GET["success"])) {
     echo "<div class='text-center alert alert-success' role='alert'>";
     if ($_GET["success"] == "regis") {
-        echo "登録完了";
+        echo "Registration Complete";
     }else if($_GET["success"]== "edit"){
-        echo "編集完了";
+        echo "Edit Complete";
     }
     echo "</div>";
 
